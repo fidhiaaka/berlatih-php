9c8b41f93a9f69904a74dc138d2f6964e96fd055
fidhiaaka <vidhiaaka7@gmail.com>
2020-07-29 21:34:22 +0700

    first commit

---
+++ cari-mean.php
@@ -0,0 +1,17 @@
+<?php
+
+function cari_mean($arr)
+{
+  //kode di sini
+  $hasil = number_format(array_sum($arr)/count($arr));
+  echo $hasil . "<br>";
+}
+
+// TEST CASE 
+echo cari_mean([1, 2, 3, 4, 5]); // 3
+echo cari_mean([3, 5, 7, 5, 3]); // 5
+echo cari_mean([6, 5, 4, 7, 3]); // 5
+echo cari_mean([1, 3, 3]); // 2
+echo cari_mean([7, 7, 7, 7, 7]); // 7
+
+?>
